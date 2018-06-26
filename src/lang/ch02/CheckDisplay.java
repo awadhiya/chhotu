14c65c6ed4d80f9f5ce59c4fea9ea830e4a56556
awadhiya <pawadhiya@gmail.com>
2018-06-26 08:59:37 -0400

    Adding

---
+++ src/lang/ch02/CheckDisplay.java
@@ -0,0 +1,14 @@
+package lang.ch02;
+
+public class CheckDisplay {
+static void checkPrefix() {
+	System.out.println(010);
+	System.out.println(0xF);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		checkPrefix();
+
+	}
+
+}
